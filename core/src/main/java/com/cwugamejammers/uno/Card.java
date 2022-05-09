d57021d242dd706bde42ef4d9b2cae9efc1e0eed
AndersonMars <Marshall.Anderson18@gmail.com>
2022-05-09 15:44:37 -0700

    Removed useless code
    Removed a lot of useless commented out code.

--- core/src/main/java/com/cwugamejammers/uno/Card.java
+++ core/src/main/java/com/cwugamejammers/uno/Card.java
@@ -14,10 +14,6 @@ public class Card implements Comparable<Card>
 {
 	// Colors
 	private final static String[] color = { "Red", "Blue", "Yellow", "Green", "Black" };
-
-	// Deck
-	//private static Stack<Card> deck;
-
 	private static ArrayList<Card> playField = new ArrayList<Card>();
 
 	// What the number of our card is (0,1,2...9)
@@ -117,51 +113,6 @@ public class Card implements Comparable<Card>
 		}
 
 		shuffle();
-		/*
-		Card card = null;
-
-		for (int i = 0; i < 4; i++) {
-			card = new Card(0, color[i], false);
-			deck.push(card);
-
-			for (int cardValue = 1; cardValue < 10; cardValue++) {
-				card = new Card(cardValue, color[i], false);
-				deck.push(card);
-				deck.push(card);
-			}
-			// 10 = skip
-			// 11 = reversal
-			// 12 = +2
-			// 13 wild
-			// 14 wild +4
-			card = new Card(10, color[i], true);
-			deck.push(card);
-			deck.push(card);
-			card = new Card(11, color[i], true);
-			deck.push(card);
-			deck.push(card);
-			card = new Card(12, color[i], true);
-			deck.push(card);
-			deck.push(card);
-
-		}
-
-		// Creates 4 Wild Cards
-		for (int i = 0; i < 4; ++i) {
-			card = new Card(13, "Wild", true);
-			deck.push(card);
-		}
-
-		// Creates 4 Wild+4 Cards
-		for (int i = 0; i < 4; ++i) {
-			card = new Card(14, "Wild", true);
-			deck.push(card);
-		}
-
-		// Shuffles
-		Collections.shuffle(deck);
-
-		 */
 	}
 
 	public static void shuffle()
@@ -198,6 +149,20 @@ public class Card implements Comparable<Card>
 
 	}
 
+	public static void drawTwo(Player player)
+	{
+		draw(player);
+		draw(player);
+	}
+
+	public static void drawFour(Player player)
+	{
+		draw(player);
+		draw(player);
+		draw(player);
+		draw(player);
+	}
+
 	public static void emptyShuffle()
 	{
 		Card temp = deck.get(deck.size() - 1);
@@ -215,122 +180,4 @@ public class Card implements Comparable<Card>
 	{
 		return deck;
 	}
-
-	/*
-	public static Stack<Card> getDeck()
-	{
-		return deck;
-	}
-
-	public static int getDeckSize()
-	{
-		return deck.size();
-	}
-
-	public void setIndex(int index)
-	{
-		this.index = index;
-	}
-
-	public int getIndex()
-	{
-		return index;
-	}
-
-	public static void cardDraw(Stack<Card> deck, Player player)
-	{
-		//If there are no cards remaining when a draw is attempted, reshuffle the deck first.
-		if(getDeckSize() == 0)
-		{
-			reshuffle();
-		}
-
-		//Draw the card
-		Card draw = deck.pop();
-		player.getList().add(draw);
-		//Sort the hand after draw
-		//player.getHand().sortHand();
-
-		if(player.getId() == 0)
-		{
-			//CREATE THE CARD BUTTON
-			PlayScreen.createCard(draw);
-		}
-	}
-
-	public static void cardDrawTwo(Stack<Card> deck, Player player)
-	{
-
-		for(int i = 0; i < 2; i++)
-		{
-			//If there are no cards remaining when a draw is attempted, reshuffle the deck first.
-			if(getDeckSize() == 0)
-			{
-				reshuffle();
-			}
-
-			//Draw the card
-			player.getList().add(deck.pop());
-
-		}
-
-		cardDraw(deck, player);
-		cardDraw(deck, player);
-		//player.getHand().sortHand();
-	}
-
-	public static void cardDrawFour(Stack<Card> deck, Player player)
-	{
-
-		for(int i = 0; i < 4; i++)
-		{
-			//If there are no cards remaining when a draw is attempted, reshuffle the deck first.
-			if(getDeckSize() == 0)
-			{
-				reshuffle();
-			}
-
-			//Draw the card
-			player.getList().add(deck.pop());
-		}
-
-		cardDraw(deck, player);
-		cardDraw(deck, player);
-		cardDraw(deck, player);
-		cardDraw(deck, player);
-		//player.getHand().sortHand();
-	}
-	*/
-	/*
-	public static void cardPlay(Card card) {
-		playField.push(card);
-	}
-
-	public static Card playFieldView()
-	{
-		return playField.peek();
-	}
-	*/
-
-	/*
-	public static void reshuffle() {
-		// Pushes the top playField card so it is safe for later
-		Card temp = playField.pop();
-
-		// Push the remaining deck into the playField
-		for (int i = 0; i < deck.size(); i++) {
-			playField.push(deck.pop());
-		}
-
-		// Move all of playField into deck
-		for (int i = 0; i < playField.size(); i++) {
-			deck.push(playField.pop());
-		}
-		// shuffle it
-		Collections.shuffle(deck);
-		// move our temp back to playField
-		playField.push(temp);
-	}
-
-	 */
 }
--- core/src/main/java/com/cwugamejammers/uno/PlayScreen.java
+++ core/src/main/java/com/cwugamejammers/uno/PlayScreen.java
@@ -47,13 +47,6 @@ public class PlayScreen implements Screen, GestureDetector.GestureListener{
         redBackground = new Texture("RedBackground.png");
         blueBackground = new Texture("BlueBackground.png");
 
-        //TO BE REMOVED TEST PURPOSES
-        //t = new Texture("TestCard.png");
-        //t2 = new Texture("cat.jpg");
-        //t3 = new Texture("upcat.jpg");
-        //addCard = new Button(t2, Gdx.graphics.getWidth() - 250, Gdx.graphics.getHeight() - 250, 250, 250);
-        //removeCard = new Button(t3, 0, Gdx.graphics.getHeight() - 250,250, 250);
-
         song1 = Gdx.audio.newMusic(Gdx.files.internal("music/AndSoItBegins.mp3"));
         song2 = Gdx.audio.newMusic(Gdx.files.internal("music/Branch.mp3"));
         song3 = Gdx.audio.newMusic(Gdx.files.internal("music/Octilary.mp3"));
@@ -84,93 +77,9 @@ public class PlayScreen implements Screen, GestureDetector.GestureListener{
 
         song1.play();
 
-        //createTextures();
-
-        //TEST STUFF, WILL BE GONE LATER
-        //card1 = new Button(t, 0, 0, Gdx.graphics.getWidth()/3, Gdx.graphics.getHeight()/3);
         cardList = new ArrayList<Button>();
-        //cardList.add(card1);
-
-        /*
-        Texture text1 = new Texture("cards/R1.PNG");
-        Texture text2 = new Texture("cards/B2.PNG");
-        Texture text3 = new Texture("cards/Y3.PNG");
-        Texture text4 = new Texture("cards/G4.PNG");
-        Texture text5 = new Texture("cards/W13.PNG");
-        Texture text6 = new Texture("cards/R2.PNG");
-        Texture text7 = new Texture("cards/B2.PNG");
-
-        Button card1 = new Button(text1, 0, 0, Gdx.graphics.getWidth()/3, Gdx.graphics.getHeight()/3);
-        Button card2 = new Button(text1, 0, 0, Gdx.graphics.getWidth()/3, Gdx.graphics.getHeight()/3);
-        Button card3 = new Button(text1, 0, 0, Gdx.graphics.getWidth()/3, Gdx.graphics.getHeight()/3);
-        Button card4 = new Button(text1, 0, 0, Gdx.graphics.getWidth()/3, Gdx.graphics.getHeight()/3);
-        Button card5 = new Button(text1, 0, 0, Gdx.graphics.getWidth()/3, Gdx.graphics.getHeight()/3);
-        Button card6 = new Button(text1, 0, 0, Gdx.graphics.getWidth()/3, Gdx.graphics.getHeight()/3);
-        Button card7 = new Button(text1, 0, 0, Gdx.graphics.getWidth()/3, Gdx.graphics.getHeight()/3);
-
-        cardList.add(card1);
-        cardList.add(card2);
-        cardList.add(card3);
-        cardList.add(card4);
-        cardList.add(card5);
-        cardList.add(card6);
-        cardList.add(card7);
-        */
-
         controller = new GameController();
     }
-
-    public void createTextures()
-    {
-
-        /*
-        textureList = new ArrayList<>();
-
-        //Creates 5 separate ArrayLists for the 5 texture lists.
-        ArrayList<Texture> red = new ArrayList<Texture>();
-
-        ArrayList<Texture> blue = new ArrayList<Texture>();
-
-        ArrayList<Texture> yellow = new ArrayList<Texture>();
-
-        ArrayList<Texture> green = new ArrayList<Texture>();
-
-        ArrayList<Texture> wild = new ArrayList<Texture>();
-
-        textureList.add(red);
-        textureList.add(blue);
-        textureList.add(yellow);
-        textureList.add(green);
-        textureList.add(wild);
-
-
-
-        for(int i = 0; i < 12; i++)
-        {
-            //red
-            Texture asset = new Texture("cards/"+"R" +Integer.valueOf(i + 1)+".PNG");
-            //blue
-            Texture asset2 = new Texture("cards/"+"B"+Integer.valueOf(i + 1)+".PNG");
-            //yellow
-            Texture asset3 = new Texture("cards/"+"Y" + Integer.valueOf(i + 1)+".PNG");
-            //green
-            Texture asset4 = new Texture("cards/"+"G" +Integer.valueOf(i + 1)+".PNG");
-
-            textureList.get(0).add(asset);
-            textureList.get(1).add(asset2);
-            textureList.get(2).add(asset3);
-            textureList.get(3).add(asset4);
-        }
-
-        Texture wild1 = new Texture("cards/W13.PNG");
-        Texture wild2 = new Texture("cards/W14.PNG");
-        //wild
-        /*
-        textureList.get(4).add(wild1);
-        textureList.get(4).add(wild2);
-        */
-    }
-
     public ArrayList<ArrayList<Texture>> getTextureList()
     {
         return textureList;
@@ -210,45 +119,6 @@ public class PlayScreen implements Screen, GestureDetector.GestureListener{
         Texture cardPicture = new Texture(fileName);
         Button cardButton = new Button(cardPicture, 0, 0, Gdx.graphics.getWidth() / 3, Gdx.graphics.getHeight() / 3);
         cardList.add(cardButton);
-
-
-
-
-        /*
-        int arrayNumber = 0;
-
-        int indexNumber = card.getNumber();
-
-        if(card.getColor() == "Red")
-        {
-            arrayNumber = 0;
-        }
-
-        else if(card.getColor() == "Blue")
-        {
-            arrayNumber = 1;
-        }
-
-        else if(card.getColor() == "Yellow")
-        {
-            arrayNumber = 2;
-        }
-
-        else if(card.getColor() == "Green")
-        {
-            arrayNumber = 3;
-        }
-
-        else if(card.getColor() == "Wild")
-        {
-            arrayNumber = 4;
-        }
-
-        Button cardButton = new Button(textureList.get(arrayNumber).get(indexNumber), 0, 0, Gdx.graphics.getWidth() / 3, Gdx.graphics.getHeight() / 3);
-        cardList.add(cardButton);
-
-         */
-
     }
     @Override
     public void show() {
@@ -287,28 +157,6 @@ public class PlayScreen implements Screen, GestureDetector.GestureListener{
         //Begins the spritebatch
         game.batch.begin();
 
-        /*
-        //TO BE REMOVED ADDS A CARD FOR TESTING
-        if (Gdx.input.justTouched()) {
-            if (addCard.collision(Gdx.input.getX(), Gdx.input.getY())) {
-                cardList.add(new Button(t, cardList.get(cardList.size() - 1).rect.x + cardList.get(cardList.size() - 1).rect.width, 0, Gdx.graphics.getWidth() / 3, Gdx.graphics.getHeight() / 3));
-            }
-
-            if (removeCard.collision(Gdx.input.getX(), Gdx.input.getY())) {
-                if(cardList.size() > 0) {
-                    cardList.remove(0);
-
-                    ////MAKE A FUNCTION OUT OF THIS
-                    cardList.get(0).reposition(0, 0);
-                    for (int i = 1; i < cardList.size(); i++){
-                        cardList.get(i).reposition(cardList.get(i-1).getx() + cardList.get(i).rect.width, cardList.get(i).rect.y);
-                    }
-                }
-            }
-        }
-         */
-
-
         //depending on the theme draw a specific color background
         if (game.colortheme == Uno.COLORTHEME.RED){
             game.batch.draw(redBackground, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
@@ -322,13 +170,6 @@ public class PlayScreen implements Screen, GestureDetector.GestureListener{
         for (Button b : cardList){
             b.draw(game.batch);
         }
-
-        //TO BE REMOVED TEST PURPOSES ONLY
-        /*
-        addCard.draw(game.batch);
-        removeCard.draw(game.batch);
-        */
-
 
         game.batch.end();
     }
